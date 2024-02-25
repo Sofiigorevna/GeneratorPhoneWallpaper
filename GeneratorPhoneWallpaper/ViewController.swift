53bf5b0b9df4f7255e8a48e73c02043f161a2078
sofiigorevna <sofia.konchencko@yandex.ru>
2024-02-25 18:06:50 +0300

    Initial Commit

---
+++ GeneratorPhoneWallpaper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeneratorPhoneWallpaper
+//
+//  Created by 1234 on 25.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
